6870a36b026b28b6161470175b7e089746b2b49e
andrewrosario <rosario.andrew@gmail.com>
2019-08-12 11:17:40 -0500

    new_review complete

--- tools/console.rb
+++ tools/console.rb
@@ -7,9 +7,21 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+arbys = Restaurant.new("Arby's")
+burger_king = Restaurant.new("Burger King")
+mcdonalds = Restaurant.new("McDonald's")
+
 all_customers = Customer.all
 all_restaurants = Restaurant.all
 all_reviews = Review.all
+
+andy = Customer.new("Andy", "Rosario")
+sarah = Customer.new("Sarah", "Bird")
+ravel = Customer.new("Ravel", "Thai")
+mason = Customer.new("Mason", "?")
+ronald = Customer.new("Ronald", "Thai")
+mom = Customer.new("Cathy", "Rosario")
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
