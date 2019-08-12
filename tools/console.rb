78aefb18659d71dbf6d40d7de9357724372b595f
andrewrosario <rosario.andrew@gmail.com>
2019-08-12 11:36:03 -0500

    finished restaurant methods

--- tools/console.rb
+++ tools/console.rb
@@ -22,6 +22,22 @@ mason = Customer.new("Mason", "?")
 ronald = Customer.new("Ronald", "Thai")
 mom = Customer.new("Cathy", "Rosario")
 
+andy.add_review(arbys, "This sucks", 1)
+andy.add_review(mcdonalds, "Amazing", 5)
+ravel.add_review(arbys, "My favorite", 5)
+andy.add_review(arbys, "Much better, but still pretty bad", 2)
+
+andy_num_reviews = andy.num_reviews
+
+andy_restaurants = andy.restaurants 
+
+arbys_customers = arbys.customers
+
+arbys_average = arbys.average_star_rating
+
+long_review = arbys.longest_review
+
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
